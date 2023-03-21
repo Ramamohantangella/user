f04c2f36d6bc4db11ca5f59bd87a8e266bd31cfc
mohan161481 <mohan161481@gmail.com>
2023-03-21 17:38:49 +0530

    first commit

---
+++ src/main/java/user/user1.java
@@ -0,0 +1,19 @@
+package user;
+
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class user1 extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+	}
+
+}
